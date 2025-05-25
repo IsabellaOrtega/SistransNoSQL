dd12492f0d73311433c832a49605caf6b0d2000d
Isabella Ortega <i.ortegal@uniandes.edu.co>
2025-05-25 18:49:47 -0500

    modelos2

--- demo/src/main/java/uniandes/edu/co/demo/modelo/Parentesco.java
+++ demo/src/main/java/uniandes/edu/co/demo/modelo/Parentesco.java
@@ -5,9 +5,11 @@ import lombok.ToString;
 @ToString
 public class Parentesco {
     private String nombre_parentesco;
+    private int documento_contribuyente;
 
-    public Parentesco(String nombre_parentesco){
+    public Parentesco(String nombre_parentesco, int documento_contribuyente){
         this.nombre_parentesco=nombre_parentesco;
+        this.documento_contribuyente=documento_contribuyente;
     }
 
     public String getNombre_parentesco(){
@@ -16,5 +18,13 @@ public class Parentesco {
     
     public void setNombre_parentesco(String nombre_parentesco){
         this.nombre_parentesco=nombre_parentesco;
+    }
+
+    public int getDocumento_contribuyente(){
+        return documento_contribuyente;
+    }
+    
+    public void setDocumento_contribuyente(int docuemnto_contribuyente){
+        this.documento_contribuyente=docuemnto_contribuyente;
     }
 }
